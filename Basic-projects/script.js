f16f1dd4173c2e3690771452b0dfb9fbf1e267c7
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-06-17 17:18:30 +0530

    In Progress

---
+++ Basic-projects/script.js
@@ -0,0 +1,87 @@
+const quizData = [
+    {
+        Question: "What is the difference between var, let, and const?",
+        a: "They are all the same.",
+b: "var is block-scoped, let and const are function-scoped.",
+c: "var is function-scoped, let and const are block-scoped.",
+d: "const can be reassigned, let cannot.",
+Correct: "c,"
+    },
+
+    {
+        Question: "Which of the following is NOT a JavaScript data type?",
+a: "Number",
+b: "String",
+c: "Float",
+d: "Boolean",
+Correct: "c",
+    },
+
+    {
+        Question: "What is the difference between == and === in JavaScript?",
+a: "Both compare values and types.",
+b: "== compares value only, === compares value and type.",
+c:  "== compares type only, === compares value only.",
+d: "There is no difference.",
+Correct: "b",
+    },
+
+    {
+
+        Question: "What is a closure in JavaScript?",
+a: "A function that is closed after use.",
+b: "A way to define variables globally.",
+c: "A function having access to its outer function’s variables even after the outer function has returned.",
+d: "A block of code that executes immediately.",
+Correct: "c",
+    },
+];
+
+
+const quiz = document.getElementById('quiz');
+const answerEll = document.getElementById('.answer');
+const questionEl = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submitBtn = document.getElementById('submit');
+
+
+let currentQuiz = 0;
+let startingScore = 0;
+
+// changed the variable from score to startingScore..
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+    questionEl.innerText = currentQuizData.question
+    a_text.innerText = currentQuizData.a
+    b_text.innerText = currentQuizData.b
+    c_text.innerText = currentQuizData.c
+    d_text.innerText = currentQuizData.d
+}
+
+
+function deselectAnswers() {
+    answerEll.forEach(answerEl => answerEl.checked = false)
+}
+
+
+function getSelected(){
+    let answer
+
+    answerEll.forEach(answerEl => {
+        if(answerEl.checked){
+            answer = answerEl.id
+        }
+    })
+
+    return answer
+}
+
+
+
+
