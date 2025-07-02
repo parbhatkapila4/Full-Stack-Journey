f2f3a8d761dbdf06eeb88783776ffb7a24e53bce
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-02 13:58:05 +0530

    -

--- Week-4/index.js
+++ Week-4/index.js
@@ -1,68 +1,65 @@
-const fs = require("fs")
-
-function main(fileName){
-fs.readFile(fileName,"utf-8", (err, data) =>{
-
-    let total = 0;
-    for(i = 0; i < data.length; i++){
-        if(data[i] === "") {
-total++;
-        }
-    }
-    console.log(total);
-})
-}
-
-main("a.txt");
-
-
-
-
-
-
-
-
-const express = require("express")
-
-function calculateSum(a,b){
-    let ans = 0;
-    for(i = 0; i <n; i++){
-        ans = ans + i;
-    }
-    return ans;
-}
-
-
+const express = require("express");
 const app = express();
 
+
+const users = [{
+    name: 'jaat',
+    kidneys: [{
+        healthy : false
+    }, {
+        healthy: true
+    }]
+}]
+
+
+app.use(express.json());
+
 app.get("/", function(req, res){
-    const n = req.query.n;
-    const app = calculateSum(n);
-    res.send(app)
-});
+    const jaatKidneys = users[0].kidneys
+    const noofKidneys = jaatKidneys.length;
+    let noofhealthykidneys = 0;
+    for(i = 0; i<jaatKidneys.length; i++){
+        if(jaatKidneys[i].healthy){
+            noofhealthykidneys = noofhealthykidneys + 1;
+        }
+
+    }
+
+
+    const noofunhealthykidneys = noofKidneys - noofhealthykidneys;
+
+    res.json({
+        noofKidneys,
+noofhealthykidneys,
+        noofunhealthykidneys
+    })
+})
+
+app.post("/", function(req, res){
+    const ishealthy = req.body.ishealthy;
+    users[0].kidneys.push({
+        healthy : ishealthy
+    })
+
+    res.json({
+        message: "It's Done Again"
+    })
+})
+
+
+
+
+app.put("/", function(req, res){
+    
+})
+
+app.delete("/", function(req, res){
+    
+})
+
 
 
 app.listen(3000);
-
-
-
-
-const express = require("express");
-
-function calculateSum2(a,b){
-    return a + b;
-}
-
-const app2 = express()
-
-app2.get("/", function(req,res){
-    const n = req.query.a;
-    const n1 = req.query.b;
-    const app2 = calculateSum2(a,b);
-    res.send(app2);
-})
-
-app.listen(3001);
 
 
 
