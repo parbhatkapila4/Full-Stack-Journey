a494d12a2aaaeb6583aa38276dd54a2cdbc95e34
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-04 00:16:21 +0530

    Middleware ride

---
+++ Week-5/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+
+function CheckerMiddleware(req, res, next){
+    const age = req.query.age;
+    if(age >=15){
+        next();
+    }else{
+        res.status(404).json({
+            mgs: "You are not applicable for this ride!"
+        })
+    }
+}
+
+
+app.get("/ride1", CheckerMiddleware, (req,res)=> {
+    res.json({
+        msg: "Go Ahead Enjoy ride 1"
+    })
+})
+
+
+
+app.get("/ride2", CheckerMiddleware, (req,res)=> {
+    res.json({
+        msg: "Yaaaa! Enjoy ride 2"
+    })
+})
+
+
+
+
+app.listen(3000);
