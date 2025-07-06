5f136f189cd6e814b8a894ee175a822e94b20130
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-06 22:23:55 +0530

    How to use Body-parser

--- Learning/index.js
+++ Learning/index.js
@@ -1,34 +1,67 @@
+// const express = require("express");
+
+// const app = express();
+
+// function middleware (req, res, next){
+//   console.log(`Method of this is ${req.method}`);
+//     console.log(`Url of this is ${req.hostname}`);
+//   console.log(new Date());
+
+//   next();
+// }
+
+// app.use(middleware);
+
+// app.get("/sum" , function(req ,res){
+//   const a = parseInt(req.query.a);
+//   const b = parseInt(req.query.b);
+
+//   res.json({
+//     ans: a+b
+//   })
+// })
+
+//   app.get("/subtract" ,function(req ,res){
+//   const a = req.query.a;
+//   const b = req.query.b;
+
+//    res.json({
+//     ans: a - b
+//   })
+//   })
+
+//   app.get("/multiply" ,function(req ,res){
+//   const a = req.query.a;
+//   const b = req.query.b;
+
+//    res.json({
+//     ans: a*b
+//   })
+// })
+//   app.get("/divide" ,function(req, res){
+//   const a = req.query.a;
+//   const b = req.query.b;
+
+//    res.json({
+//     ans: a/b
+//   })
+
+// })
+
+// app.listen(3000);
+
 const express = require("express");
+const bodyparser = require ("body-parser");
 const app = express();
 
-app.get("/sum/:a/:b", (req, res)=>{
-    const a = parseInt(req.params.a);
-    const b  =parseInt(req.params.b);
+app.use(bodyparser.json());
 
-    res.json({
-        ans: a + b
-    })
-})
+app.post("/sum", function (req, res) {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
 
-
-app.get("/subtract/:a/:b", (req, res) => {
-  const {a,b} = req.params;
   res.json({
-    ans: a - b,
-  });
-});
-
-app.get("/multiply/:a/:b", (req, res) => {
-  const {a,b} = req.query;
-  res.json({
-    ans: a * b,
-  });
-});
-
-app.get("/divide/:a/:b", (req, res) => {
-  const {a,b} = req.query;
-  res.json({
-    ans: a / b,
+    ans: a + b,
   });
 });
 
