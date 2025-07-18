42402292d1701b7915b35fe87e6e5b03b5d525ab
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-18 17:05:05 +0530

    Component Inline Styling

---
+++ Week-9/React-basics/src/posts.jsx
@@ -0,0 +1,16 @@
+function PostComponent(){
+    return(
+        <div>
+            name:"Parbhat Kapila"
+            <div>
+                subtitle: "200000 followers"
+            </div>
+            <div>
+                time: 22
+            </div>
+        </div>
+    )
+}
+
+
+export default PostComponent;
