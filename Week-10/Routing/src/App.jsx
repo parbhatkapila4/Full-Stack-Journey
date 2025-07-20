bfe9fba0cdd5599be4254855f499b713c2ecd579
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-20 17:36:32 +0530

    Context Api Jargon

--- Week-10/Routing/src/App.jsx
+++ Week-10/Routing/src/App.jsx
@@ -1,119 +1,59 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link,
-  useNavigate,
-  Outlet} from "react-router-dom";
+import { useState, createContext } from "react"
 
 
+const BulbComponent = createContext();
 
-function App() {
-  return (
+const App = () =>{
+const [bulbOn, setBulbOn] = useState(true);
+  return(
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Toppage />}>
-          <Route
-            path="/neet/online-coaching-class-11"
-            element={<Class11Program />}
-          />
-          <Route
-            path="/neet/online-coaching-class-12"
-            element={<Class12Program />}
-          />
-          <Route path="/Basics" element={<LandingComponent />} />
-          <Route path="*" element={<ErrorPage />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <BulbComponent value={{
+        bulbOn:bulbOn,
+        setBulbOn:setBulbOn
+      }}>
+      <Light/>
+      </BulbComponent>
     </div>
-  );
+  )
+}
+function Light({bulbOn, setBulbOn}){
+  
+  return(
+    <div>
+      <LightBulb bulbOn={bulbOn}/>
+      <LightSwitch bulbOn={bulbOn} setBulbOn={setBulbOn}/>
+    </div>
+  )
+}
 
-  function Toppage() {
-    return (
-      <div style={{height: "100vh", background: "green"}}>
-      <Headers />
-      <div style={{height: "90vh", background: "orange"}}>
-        <Outlet />
-      </div>
-      Footers
-      </div>
-    );
-  }
-
-  function Headers(){
+  function LightBulb({bulbOn}){
+    
     return(
-      <div >
- <Link to="/neet/online-coaching-class-11">Class 11th</Link>
-        <br></br>
-        <Link to="/neet/online-coaching-class-12">Class 12th</Link>
-        <br></br>
-        <Link to="/">Allen</Link>
+      <div>
+{bulbOn ?  "Bulb On" : "Bulb Off"}
       </div>
     )
   }
 
-  function LandingComponent() {
-    return (
-      <div
-        style={{
-          backgroundColor: "yellow",
-          color: "black",
-          padding: 20,
-          margin: 20,
-          borderRadius: 30,
-        }}
-      >
-        Hey There Thanks for Showing up at Allen's landing page!
-      </div>
-    );
-  }
+  function LightSwitch({bulbOn, setBulbOn}){
 
-  function Class11Program() {
-    const navigate = useNavigate();
-    function RedirectUser() {
-      navigate("/");
+    function toggle(){
+setBulbOn(!bulbOn)
     }
-    return (
-      <div
-        style={{
-          backgroundColor: "pink",
-          color: "black",
-          padding: 20,
-          margin: 20,
-          borderRadius: 20,
-        }}
-      >
-        NEET PROGRAMS FOR 11th CLASS!
-        <button onClick={RedirectUser}>GO Back!</button>
+
+      
+      
+      
+    return(
+      
+      <div>
+        <button onClick={toggle}>Toggle Button For Bulb</button>
+        
       </div>
-    );
+    )
   }
 
-  function Class12Program() {
-    return (
-      <div
-        style={{
-          backgroundColor: "orange",
-          color: "black",
-          padding: 20,
-          margin: 20,
-          borderRadius: 20,
-        }}
-      >
-        NEET PROGRAMS FOR 12th CLASS!
-      </div>
-    );
-  }
-}
+  export default App;
 
-function ErrorPage() {
-  return (
-    <div style={{ background: "red" }}>
-      Sorry Page dont't found Kindly Redirect
-    </div>
-  );
-}
 
-export default App;
+
