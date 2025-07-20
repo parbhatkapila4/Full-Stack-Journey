703293883fa85e5a0f38e43210e89b565f4081de
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-20 21:28:28 +0530

    Custom Counter Hook

--- Week-11/Custom Hooks/src/App.jsx
+++ Week-11/Custom Hooks/src/App.jsx
@@ -1,8 +1,40 @@
-const App = ()=>{
+import { useState } from "react";
 
+
+
+
+function useCounter(){
+  const[count, setcount] = useState(0);
+
+
+  function increasecount(){
+setcount(count => count + 1);
+  }
+  return{
+    Count: count,
+    Increasecount: increasecount
+
+  }
+}
+const App = ()=>{
   return(
     <div>
-      Hey there
+      <Counter />
+            <Counter />
+
+      <Counter />
+
+      <Counter />
+
+    </div>
+  )
+}
+
+function Counter() {
+  const {Count, Increasecount} = useCounter()
+  return(
+    <div>
+      <button onClick={Increasecount}>Increase {Count}</button>
     </div>
   )
 }
