6bf8b8d127e7c61c0e91e2060e987772aa01e6ed
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-22 19:59:22 +0530

    Custom useFetch Hook

--- Week-11/Custom Hooks/src/App.jsx
+++ Week-11/Custom Hooks/src/App.jsx
@@ -1,41 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { usePostTitle } from "./hooks/useFetch";
+
+const App = () => {
+  const poststitle = usePostTitle();
 
 
-
-
-function useCounter(){
-  const[count, setcount] = useState(0);
-
-
-  function increasecount(){
-setcount(count => count + 1);
-  }
-  return{
-    Count: count,
-    Increasecount: increasecount
-
-  }
-}
-const App = ()=>{
   return(
     <div>
-      <Counter />
-            <Counter />
+      {poststitle}
+     </div>
 
-      <Counter />
-
-      <Counter />
-
-    </div>
-  )
-}
-
-function Counter() {
-  const {Count, Increasecount} = useCounter()
-  return(
-    <div>
-      <button onClick={Increasecount}>Increase {Count}</button>
-    </div>
   )
 }
 
---
+++ Week-11/Custom Hooks/src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useEffect, useState } from "react";
+
+export function usePostTitle(){
+      const[posts, setPosts] = useState({});
+    
+    async function getPosts() {
+      const response = await fetch("https://jsonplaceholder.typicode.com/posts/1");
+      const json = await response.json();
+      setPosts(json)
+    }
+     useEffect(() => {
+getPosts()
+  }, [])
+
+  return posts.title;
+  
+}
