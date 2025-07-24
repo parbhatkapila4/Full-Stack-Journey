d20d970285de6e13feca580d6265994bcabe7b77
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-07-25 00:21:40 +0530

    Updates

--- Week-5/Middleware-Learning/practice.js
+++ Week-5/Middleware-Learning/practice.js
@@ -1,28 +1,28 @@
 // How to use mapping for transforming of array ----------->
 
-// const input =  [1,2,3,4,5];
+const input =  [1,2,3,4,5];
 
-// function transform(i){
-//     return i * 2;
-// }
+function transform(i){
+    return i * 2;
+}
 
-// const NewVaalue = input.map(transform);
+const NewVaalue = input.map(transform);
 
-// console.log(NewValue);
+console.log(NewValue);
 
 // How to use filter to find out even value numbers from given array ------->
-// const arr = [1,2,3,56,7,89755,3534534];
+const arr = [1,2,3,56,7,89755,3534534];
 
-// function LogicFilter(num){
-//     if(num%2===0){
-//         return true;
-//     }else
-//     return false;
-// }
+function LogicFilter(num){
+    if(num%2===0){
+        return true;
+    }else
+    return false;
+}
 
-// const LastValue = arr.filter(LogicFilter);
+const LastValue = arr.filter(LogicFilter);
 
-// console.log(LastValue);
+console.log(LastValue);
 
 
 
