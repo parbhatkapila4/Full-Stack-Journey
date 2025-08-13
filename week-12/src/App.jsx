140c79cc530da506df6360bb5dff53116ae65121
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-13 18:40:32 +0530

    Toggling Conditionally

--- week-12/src/App.jsx
+++ week-12/src/App.jsx
@@ -2,39 +2,8 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="bg-gray-500 h-screen">
+    <div className="bg-gray-500 border-4 border-white h-screen">
       <ToggleMessage />
-      <Greetings name={"Ramesh"} />
-      <div className="flex justify-center">
-        <div>
-          <div className="bg-white mt-7 rounded-2xl p-10 w-[300px]">
-            <PostComponent
-              name={"Parbhat"}
-              subtitle={"20 Followers"}
-              time={"2m ago"}
-              image={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
-              }
-              description={
-                "Want to know how to win Big? Check out how these folks won $6000 in Bounties"
-              }
-            />
-          </div>
-          <br></br>
-          <div className="bg-white mt-7 rounded-2xl p-10 w-[300px]">
-            <PostComponent
-              name={"Parbhat"}
-              subtitle={"promoted"}
-              image={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
-              }
-              description={
-                "Want to know how to win Big? Check out how these folks won $6000 in Bounties"
-              }
-            />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
@@ -54,34 +23,66 @@ function ToggleMessage() {
   );
 }
 
-function PostComponent({ name, subtitle, time, image, description }) {
-  return (
-    <div>
-      <div className="flex">
-        <img src={image} className="w-[30px] h-[30px] rounded-[30px]" />
-        <div className="text-[10px] ml-10">
-          <b>{name}</b>
-          <div>{subtitle}</div>
-          {time !== undefined ? (
-            <div className="flex">
-              <div>{time}</div>
-              <img
-                src={
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
-                }
-                className="w-[12px] h-[12px] ml-2"
-              />
-            </div>
-          ) : null}
-        </div>
-      </div>
-      <div className="text-[12px]">{description}</div>
-    </div>
-  );
-}
+// function PostComponent({ name, subtitle, time, image, description }) {
+//   return (
+//     <div>
+//       <div className="flex">
+//         <img src={image} className="w-[30px] h-[30px] rounded-[30px]" />
+//         <div className="text-[10px] ml-10">
+//           <b>{name}</b>
+//           <div>{subtitle}</div>
+//           {time !== undefined ? (
+//             <div className="flex">
+//               <div>{time}</div>
+//               <img
+//                 src={
+//                   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
+//                 }
+//                 className="w-[12px] h-[12px] ml-2"
+//               />
+//             </div>
+//           ) : null}
+//         </div>
+//       </div>
+//       <div className="text-[12px]">{description}</div>
+//     </div>
+//   );
+// }
 
-const Greetings = ({ name }) => {
-  return <h1>Hello, {name}</h1>;
-};
+// const Greetings = ({ name }) => {
+//   return <h1>Hello, {name}</h1>;
+// };
+
+//  <Greetings name={"Ramesh"} />
+//       <div className="flex justify-center">
+//         <div>
+//           <div className="bg-white mt-7 rounded-2xl p-10 w-[300px]">
+//             <PostComponent
+//               name={"Parbhat"}
+//               subtitle={"20 Followers"}
+//               time={"2m ago"}
+//               image={
+//                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
+//               }
+//               description={
+//                 "Want to know how to win Big? Check out how these folks won $6000 in Bounties"
+//               }
+//             />
+//           </div>
+//           <br></br>
+//           <div className="bg-white mt-7 rounded-2xl p-10 w-[300px]">
+//             <PostComponent
+//               name={"Parbhat"}
+//               subtitle={"promoted"}
+//               image={
+//                 "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTw_HeSzHfBorKS4muw4IIeVvvRgnhyO8Gn8w&s"
+//               }
+//               description={
+//                 "Want to know how to win Big? Check out how these folks won $6000 in Bounties"
+//               }
+//             />
+//           </div>
+//         </div>
+//       </div>
 
 export default App;
