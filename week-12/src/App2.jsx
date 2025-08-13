ebda12481fefc0ca3a8fef4c21151c41278faf4e
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-13 23:41:00 +0530

    api call via button

---
+++ week-12/src/App2.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const App2 = () => {
+  const [trade, setTrade] = useState("");
+  const [counter, setCounter] = useState(1);
+
+  useEffect(() => {
+    async function makhi() {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/todos/" + counter
+      );
+      setTrade(response.data.title);
+    }
+    makhi();
+  }, [counter]);
+  return (
+    <div>
+      {trade}
+      <button onClick={() => setCounter(counter + 1)}>Counter</button>
+      {counter}
+    </div>
+  );
+};
+export default App2;
