2876c386e3bc01486ef1158f27d91c5037168a6a
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-30 21:55:05 +0530

    Connecting Backend to FE

---
+++ Week-19/app/api/v1/user/details/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export function GET() {
+  return NextResponse.json({
+    name: "parbhat",
+    email: "parbhatkapila4@gmail.com",
+  });
+}
+
+export function POST() {
+  return NextResponse.json({
+    name: "parbhat",
+    email: "parbhatkapila4@gmail.com",
+  });
+}
+
+export function PUT() {
+  return NextResponse.json({
+    name: "parbhat",
+    email: "parbhatkapila4@gmail.com",
+  });
+}
