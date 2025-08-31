25872e9f77e5b7e8e8970480839fff47216ac5b9
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-08-31 11:21:36 +0530

    Updates

--- Week-19/app/page.tsx
+++ Week-19/app/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Home() {
-  return(
-    <div>
-      Hi there
+  return (
+    <div className="text-lg h-screen w-screen flex items-center justify-center">
+      <div>
+        Todo Application
+        <br/>
+        <Link className="bg-amber-600 text-2xl border-4 border-dotted" href="/signin">Signin to Our Todo Application</Link>
+        <br/>
+        <Link className="bg-amber-950 text-2xl border-4 border-dotted m-2" href="/signup">Signup to Our Todo Application</Link>
+      </div>
     </div>
-  )
+  );
 }
