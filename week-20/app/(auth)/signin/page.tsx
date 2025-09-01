945b3891855e41e9ab05a7b0d4f5c67f297705d9
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-09-01 09:52:12 +0530

    Route Group NextJs

---
+++ week-20/app/(auth)/signin/page.tsx
@@ -0,0 +1,10 @@
+export default function signin() {
+  return (
+    <div>
+      Signin
+      <input type="text" placeholder="Username"></input>
+      <input type="password" placeholder="password"></input>
+      <button>Sigin</button>
+    </div>
+  );
+}
---
+++ week-20/app/(auth)/signup/page.tsx
@@ -0,0 +1,10 @@
+export default function signup() {
+  return (
+    <div>
+      SignUp
+      <input type="text" placeholder="Username"></input>
+      <input type="password" placeholder="password"></input>
+      <button>Sigup</button>
+    </div>
+  );
+}
