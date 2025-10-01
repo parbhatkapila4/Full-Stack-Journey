83cd091fb3bd3a06614aab81648e59cad893cf83
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-10-01 18:23:40 +0530

    Little Changes

--- week-21/app/api/auth/[...nextauth]/route.ts
+++ week-21/app/api/auth/[...nextauth]/route.ts
@@ -7,24 +7,29 @@ const handler = NextAuth({
       name: "Login With Email",
 
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "parbhat@gmail.com" },
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "parbhat@gmail.com",
+        },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
-       const username = credentials?.username;
-       const password = credentials?.password
+        const username = credentials?.username;
+        const password = credentials?.password;
+        console.log(username);
+        console.log(password);
 
-
-       const user = {
-        name: "parbhat",
-        id: "1",
-        username: "parbhatkapila4@gmail.com"
-       }
-       if(user){
-        return user;
-       }else{
-        return null;
-       }
+        const user = {
+          name: "parbhat",
+          id: "1",
+          username: "parbhatkapila4@gmail.com",
+        };
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
       },
     }),
   ],
